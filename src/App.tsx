ecb65da4a24917d363880260e9ae4e9c9a8b18d9
Deevyne99 <kaludivine545@gmail.com>
2025-07-07 11:50:37 +0100

    Added a few changes

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <div className='flex flex-col max-w-[300px]  mx-auto justify-center items-center '>
       <img src={logo} alt=""  className='w-full flex object-cover object-center'/>
       <h1 className='text-3xl text-center mt-4'>Coming Soon...</h1>
-      <p>hello world</p>
+     
       </div>
      </div>
     </>
