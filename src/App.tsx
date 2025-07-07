0a5fb76c3d3756fcb85b2451e5bc7aa07b0a2aec
Deevyne99 <kaludivine545@gmail.com>
2025-07-07 19:53:15 +0100

    updated the site

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
         className={`w-full flex object-cover object-center ${imgLoaded ? '' : 'hidden'}`}
         onLoad={() => setImgLoaded(true)}
       />
-      <a href="#">Contact us at info@onola.org</a>
+      <a href="#" className='mt-4'>Contact us at info@onola.org</a>
       {/* <h1 className='text-3xl text-center mt-4'>Coming Soon...</h1> */}
      
       </div>
