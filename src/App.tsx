2bf33b3bcc6c27a5cda3c632ba7bea04ff25df1c
Deevyne99 <kaludivine545@gmail.com>
2025-07-07 12:17:42 +0100

    Added Skeleton Component for loading image

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 
 import './index.css'
 import logo from './assets/Onola.png'
+import { useState } from 'react'
+import { Skeleton } from './components/ui/skeleton'
+
 function App() {
+  const [imgLoaded, setImgLoaded] = useState(false)
 
 
   return (
     <>
      <div className=' flex justify-center items-center h-screen'>
       <div className='flex flex-col max-w-[300px]  mx-auto justify-center items-center '>
-      <img src={logo} alt=""  className='w-full flex object-cover object-center'/>
+      {!imgLoaded && (
+        <Skeleton className="w-full h-[200px] rounded-md mb-2" />
+      )}
+      <img
+        src={logo}
+        alt="Onola logo"
+        className={`w-full flex object-cover object-center ${imgLoaded ? '' : 'hidden'}`}
+        onLoad={() => setImgLoaded(true)}
+      />
       <h1 className='text-3xl text-center mt-4'>Coming Soon...</h1>
      
       </div>
