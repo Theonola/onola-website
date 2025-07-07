e996a3e3a84d47dadecdcfc58a6c6271d2003355
Deevyne99 <kaludivine545@gmail.com>
2025-07-07 10:53:23 +0100

    Setting up repo

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './index.css'
+import logo from './assets/Onola.png'
+function App() {
+
+
+  return (
+    <>
+     <div className=' flex justify-center items-center h-screen'>
+      <div className='flex flex-col max-w-[300px]  mx-auto justify-center items-center '>
+      <img src={logo} alt=""  className='w-full flex object-cover object-center'/>
+      <h1 className='text-3xl text-center mt-4'>Coming Soon...</h1>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
