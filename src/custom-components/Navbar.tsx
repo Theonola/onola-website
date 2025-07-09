ca4a11893f7b54d75d97cf06ba0e87b187ae7895
Deevyne99 <kaludivine545@gmail.com>
2025-07-09 10:23:10 +0100

    Created Navbar Component

--- src/custom-components/Navbar.tsx
+++ src/custom-components/Navbar.tsx
@@ -31,21 +31,21 @@ const Navbar = () => {
   //#fcb816
   return (
     <div
-      className={`flex flex-col  fixed z-40 top-0 left-0 w-full bg-[#fff] transition-shadow ${
-        isScrolled ? 'shadow-md' : ''
+      className={`flex flex-col  fixed z-40 top-0 left-0 w-full  transition-shadow bg-[var(--primary-yellow)] ${
+        isScrolled ? 'shadow-b-md' : ''
       }`}
-    >
-      <nav className='flex flex-col'>
-        <ul className='flex flex-row justify-between items-center  mx-4 md:mx-12'>
-          <Link to={'/'}>
-          <img src={logo} alt='logo' className='w-[70px]' />
+    > 
+      <nav className='flex flex-col ] shadow-md'>
+        <ul className='flex flex-row justify-between items-center  '>
+          <Link to={'/'} className='w-1/2 bg-white pl-4 md:pl-12 '>
+          <img src={logo} alt='logo' className='w-[70px] bg-transparent' /> 
           </Link>
-          <div className='sm:flex flex-row items-center justify-center hidden'>
+          <div className='w-1/2 sm:flex flex-row items-center justify-center hidden  pr-4 md:pr-12'>
             {links.map((link) => (
               <li key={link.id} className='mr-4'>
                 <Link
                   to={link.url}
-                  className='text-gray-800 hover:text-blue-500 capitalize'
+                  className='text-gray-800 hover:text-white capitalize mx-4'
                 >
                   {link.name}
                 </Link>
